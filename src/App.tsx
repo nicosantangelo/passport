8e60a8df720992cbf5e81db5f179824ae8c74d24
milanga <santangelonicolas@gmail.com>
2026-03-17 18:55:19 -0300

    generate and show form

--- src/App.tsx
+++ src/App.tsx
@@ -56,40 +56,42 @@ function generateIdentity(): Identity {
 }
 
 function App() {
+  const [identity, setIdentity] = useState<Identity | null>(null);
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <div>
-        <h1 className="mt-7 mb-7 font-medium">Identity</h1>
-        <IdentityForm />
+      <div className="w-2xl flex flex-col gap-6">
+        <h1 className="mt-7 mb-4 font-medium">Identity</h1>
+
+        <Button
+          className="w-full"
+          onClick={() => setIdentity(generateIdentity())}
+        >
+          {identity ? "REGENERATE" : "GENERATE"}
+        </Button>
+
+        {identity && <Identity identity={identity} onChange={setIdentity} />}
       </div>
     </div>
   );
 }
 
-function IdentityForm() {
-  const [identity, setIdentity] = useState<Identity>({
-    firstName: "",
-    lastName: "",
-    phone: "",
-    address: "",
-    city: "",
-    state: "",
-    zip: "",
-    dob: "",
-    username: "",
-    password: "",
-  });
-
+function Identity({
+  identity,
+  onChange,
+}: {
+  identity: Identity;
+  onChange: (identity: Identity) => void;
+}) {
   return (
-    <div className="w-2xl border border-neutral-500 rounded-md p-7 flex flex-col gap-6">
+    <div className="border border-neutral-500 rounded-md p-7 flex flex-col gap-6">
       <div className="flex flex-col gap-1.5">
         <Label htmlFor="username">Username</Label>
         <Input
           type="text"
           id="username"
-          placeholder="johndoe"
           value={identity.username}
-          onChange={(e) => setIdentity({ ...identity, username: e.target.value })}
+          onChange={(e) => onChange({ ...identity, username: e.target.value })}
         />
       </div>
 
@@ -98,9 +100,8 @@ function IdentityForm() {
         <Input
           type="text"
           id="password"
-          placeholder="••••••••"
           value={identity.password}
-          onChange={(e) => setIdentity({ ...identity, password: e.target.value })}
+          onChange={(e) => onChange({ ...identity, password: e.target.value })}
         />
       </div>
 
@@ -110,9 +111,10 @@ function IdentityForm() {
           <Input
             type="text"
             id="first-name"
-            placeholder="John"
             value={identity.firstName}
-            onChange={(e) => setIdentity({ ...identity, firstName: e.target.value })}
+            onChange={(e) =>
+              onChange({ ...identity, firstName: e.target.value })
+            }
           />
         </div>
         <div className="flex flex-col gap-1.5 flex-1">
@@ -120,9 +122,10 @@ function IdentityForm() {
           <Input
             type="text"
             id="last-name"
-            placeholder="Doe"
             value={identity.lastName}
-            onChange={(e) => setIdentity({ ...identity, lastName: e.target.value })}
+            onChange={(e) =>
+              onChange({ ...identity, lastName: e.target.value })
+            }
           />
         </div>
       </div>
@@ -132,9 +135,8 @@ function IdentityForm() {
         <Input
           type="text"
           id="phone-number"
-          placeholder="+1 (555) 000-0000"
           value={identity.phone}
-          onChange={(e) => setIdentity({ ...identity, phone: e.target.value })}
+          onChange={(e) => onChange({ ...identity, phone: e.target.value })}
         />
       </div>
 
@@ -143,9 +145,8 @@ function IdentityForm() {
         <Input
           type="text"
           id="address"
-          placeholder="123 Main St"
           value={identity.address}
-          onChange={(e) => setIdentity({ ...identity, address: e.target.value })}
+          onChange={(e) => onChange({ ...identity, address: e.target.value })}
         />
       </div>
 
@@ -155,9 +156,8 @@ function IdentityForm() {
           <Input
             type="text"
             id="city"
-            placeholder="New York"
             value={identity.city}
-            onChange={(e) => setIdentity({ ...identity, city: e.target.value })}
+            onChange={(e) => onChange({ ...identity, city: e.target.value })}
           />
         </div>
         <div className="flex flex-col gap-1.5 w-24">
@@ -165,9 +165,8 @@ function IdentityForm() {
           <Input
             type="text"
             id="state"
-            placeholder="NY"
             value={identity.state}
-            onChange={(e) => setIdentity({ ...identity, state: e.target.value })}
+            onChange={(e) => onChange({ ...identity, state: e.target.value })}
           />
         </div>
         <div className="flex flex-col gap-1.5 w-28">
@@ -175,9 +174,8 @@ function IdentityForm() {
           <Input
             type="text"
             id="zip-code"
-            placeholder="10001"
             value={identity.zip}
-            onChange={(e) => setIdentity({ ...identity, zip: e.target.value })}
+            onChange={(e) => onChange({ ...identity, zip: e.target.value })}
           />
         </div>
       </div>
@@ -187,15 +185,10 @@ function IdentityForm() {
         <Input
           type="text"
           id="date-of-birth"
-          placeholder="MM/DD/YYYY"
           value={identity.dob}
-          onChange={(e) => setIdentity({ ...identity, dob: e.target.value })}
+          onChange={(e) => onChange({ ...identity, dob: e.target.value })}
         />
       </div>
-
-      <Button className="w-full mt-3" onClick={() => setIdentity(generateIdentity())}>
-        GENERATE
-      </Button>
     </div>
   );
 }
